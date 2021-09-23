d312025c782baf43b4adb6893f1a03b7f8c59145
Владислав <vladrashkevich@mail.ru>
2021-09-23 07:51:25 +0300

    myproject

---
+++ bloggers/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BloggersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bloggers'
+    verbose_name = 'Блоггеры'
---
+++ bloggers/views.py
@@ -0,0 +1,113 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
+
+from .forms import *
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
+
+
+class BloggersHome(ListView):
+    model = Bloggers
+    template_name = 'bloggers/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Bloggers.objects.filter(is_published=True)
+
+
+
+# def index(request):
+#     posts = Bloggers.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'bloggers/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'bloggers/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+    return render(request, 'bloggers/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Bloggers, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'bloggers/post.html', context=context)
+
+
+class BloggersCategory(ListView):
+    model = Bloggers
+    template_name = 'bloggers/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Bloggers.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat.id
+        return context
+
+# def show_category(request, cat_id):
+#     posts = Bloggers.objects.filter(cat_id=cat_id)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'cat_selected': cat_id,
+#     }
+#
+#     return render(request, 'bloggers/index.html', context=context)
